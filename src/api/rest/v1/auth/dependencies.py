346a95b986dffb32292a3bd0352578b9ca67581d
Roman505050 <myhun59@gmail.com>
2024-10-12 21:13:59 +0300

    Change getting Client IP.

--- src/api/rest/v1/auth/dependencies.py
+++ src/api/rest/v1/auth/dependencies.py
@@ -1,10 +1,11 @@
-from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends, Request
+from sqlalchemy.ext.asyncio import AsyncSession
 from user_agents import parse  # type: ignore
 
 from application.session.services.session_service import SessionService
 from application.user.interfaces.services.email import IEmailService
 from application.user.services.user_service import UserService
+from config import rabbitmq_settings as settings
 from domain.session.value_objects.user_agent import UserAgent
 from infrastructure.database.connext import get_session_dependency
 from infrastructure.modules.session.repositories.session import (
@@ -14,15 +15,9 @@ from infrastructure.modules.user.repositories.user import UserRepository
 from infrastructure.services.cryptography import CryptographyService
 from infrastructure.services.email_service import RabbitMQEmailService
 
-from config import rabbitmq_settings as settings
-
 
 def get_ip_remote(request: Request) -> str:
-    return (
-        request.client.host
-        if request.client
-        else request.headers.get("X-Forwarded-For", "Unknown")
-    )
+    return request.headers.get("X-Real-IP", request.client.host)
 
 
 def get_user_agent(request: Request) -> UserAgent:
